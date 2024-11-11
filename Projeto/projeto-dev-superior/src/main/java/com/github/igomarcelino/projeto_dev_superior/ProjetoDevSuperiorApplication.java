e886613fb56188aaad17fcf6cb31591f647429e1
Igo Marcelino <geslucian@hotmail.com>
2024-11-11 19:30:06 -0300

    Realizando primeiras configuracoes

---
+++ Projeto/projeto-dev-superior/src/main/java/com/github/igomarcelino/projeto_dev_superior/ProjetoDevSuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.github.igomarcelino.projeto_dev_superior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDevSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDevSuperiorApplication.class, args);
+	}
+
+}
---
+++ Projeto/projeto-dev-superior/src/test/java/com/github/igomarcelino/projeto_dev_superior/ProjetoDevSuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.igomarcelino.projeto_dev_superior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDevSuperiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
