ec1bccac3de52693afd68d35a3d2cb5018a3fbc6
Igo Marcelino <geslucian@hotmail.com>
2024-11-12 19:39:59 -0300

    Adicionado findById que retorna um game com toda sua descricao

--- Projeto/projeto-dev-superior/src/main/java/com/github/igomarcelino/projeto_dev_superior/controllers/GameController.java
+++ Projeto/projeto-dev-superior/src/main/java/com/github/igomarcelino/projeto_dev_superior/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.github.igomarcelino.projeto_dev_superior.controllers;
 
+import com.github.igomarcelino.projeto_dev_superior.dto.GameDTO;
 import com.github.igomarcelino.projeto_dev_superior.dto.GameMinDTO;
 import com.github.igomarcelino.projeto_dev_superior.entities.Game;
 import com.github.igomarcelino.projeto_dev_superior.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,11 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll(){
         return gameService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        return gameService.findById(id);
     }
 
 }
--- Projeto/projeto-dev-superior/src/main/java/com/github/igomarcelino/projeto_dev_superior/services/GameService.java
+++ Projeto/projeto-dev-superior/src/main/java/com/github/igomarcelino/projeto_dev_superior/services/GameService.java
@@ -1,10 +1,12 @@
 package com.github.igomarcelino.projeto_dev_superior.services;
 
+import com.github.igomarcelino.projeto_dev_superior.dto.GameDTO;
 import com.github.igomarcelino.projeto_dev_superior.dto.GameMinDTO;
 import com.github.igomarcelino.projeto_dev_superior.entities.Game;
 import com.github.igomarcelino.projeto_dev_superior.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,11 +18,23 @@ public class GameService {
     /**
      * Aqui estamos retornando o DTO, seguindo o padrao de projeto
      * */
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         return gameRepository.
                 findAll().
                 stream().
                 map(game -> new GameMinDTO(game)).
                 toList();
+    }
+
+    /**
+     * Aqui retornara um game com toda sua descricao
+     * */
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+       return gameRepository.
+               findById(id).
+               map(game -> new GameDTO(game)).
+               get();
     }
 }
