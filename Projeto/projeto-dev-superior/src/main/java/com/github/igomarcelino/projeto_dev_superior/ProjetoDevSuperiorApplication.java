2710297ab867c56400615262cf55be0111f7b127
Igo Marcelino <geslucian@hotmail.com>
2024-11-21 19:57:55 -0300

    Adiciona o Server em ApiDefinition

--- Projeto/projeto-dev-superior/src/main/java/com/github/igomarcelino/projeto_dev_superior/ProjetoDevSuperiorApplication.java
+++ Projeto/projeto-dev-superior/src/main/java/com/github/igomarcelino/projeto_dev_superior/ProjetoDevSuperiorApplication.java
@@ -2,11 +2,13 @@ package com.github.igomarcelino.projeto_dev_superior;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.Servers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "DSlist-intensivao-Java-Spring", version = "1", description = "Api desenvolvida na semana intensivao Java Spring"))
+@OpenAPIDefinition(servers = {@Server(url = "/",description = "default Server URL")},info = @Info(title = "DSlist-intensivao-Java-Spring", version = "1", description = "Api desenvolvida na semana intensivao Java Spring"))
 public class ProjetoDevSuperiorApplication {
 
 	public static void main(String[] args) {
