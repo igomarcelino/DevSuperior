6afdff3eb5190a0e0e74e0b8f01cdf88417122a3
Igo Marcelino <geslucian@hotmail.com>
2024-11-21 20:05:16 -0300

    Adiciona comentario na correcao dos cors na classe main da application

--- Projeto/projeto-dev-superior/src/main/java/com/github/igomarcelino/projeto_dev_superior/ProjetoDevSuperiorApplication.java
+++ Projeto/projeto-dev-superior/src/main/java/com/github/igomarcelino/projeto_dev_superior/ProjetoDevSuperiorApplication.java
@@ -8,6 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/**
+ * Corrige o problema de cors, aqui ele aponta para o servidor em que o swagger estara rodando,
+ * baseado em sua url raiz. Ele acontece esse erro pois o railway sobe em https e o swagger sobe em http.
+ * */
 @OpenAPIDefinition(servers = {@Server(url = "/",description = "default Server URL")},info = @Info(title = "DSlist-intensivao-Java-Spring", version = "1", description = "Api desenvolvida na semana intensivao Java Spring"))
 public class ProjetoDevSuperiorApplication {
 
