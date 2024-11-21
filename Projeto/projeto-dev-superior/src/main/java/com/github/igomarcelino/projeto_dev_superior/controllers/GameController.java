d17bbb40b66a4a67fa4e14489585b88ed1375e24
Igo Marcelino <geslucian@hotmail.com>
2024-11-21 19:26:19 -0300

    Adicionando Cross Origin

--- Projeto/projeto-dev-superior/src/main/java/com/github/igomarcelino/projeto_dev_superior/controllers/GameController.java
+++ Projeto/projeto-dev-superior/src/main/java/com/github/igomarcelino/projeto_dev_superior/controllers/GameController.java
@@ -9,16 +9,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.tags.Tags;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/games")
 @Tag(name = "Game")
+@CrossOrigin
 public class GameController {
 
     @Autowired
